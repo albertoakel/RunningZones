31606de65efe794d891b6c7b398b2c77fdd1bc48
albertoakel <alberto.akel@gmail.com>
2025-01-13 12:15:23 -0300

    - updates

--- gps_files_teste.py
+++ gps_files_teste.py
@@ -5,138 +5,124 @@ Created on Wed Jul 10 15:59:37 2024
 
 @author: akel
 """
+def gpxfile_imp2(filegpx,**kwargs):
+    """
+    :param filegpx: Extract data info of gpx file from strava
+    :return: time,distance e pace
+    """
 
-#import gpxpy
+    import gpxpy.gpx
+    from datetime import timezone, timedelta
+    from haversine import haversine
+    opt = kwargs.get('opt')
+    if opt is None:
+        pass
+    plt.close('all')
+
+    gpx_file = open(filegpx, 'r')
+    gpx0 = gpxpy.parse(gpx_file)
+
+    #moving_data = gpx0.get_moving_data(raw=True)
+    #print(moving_data)
+
+
+    hr = []  #  batimento cardíaco
+    lat = []  # latitude
+    lon = []  # longitude
+    ele = []  # elevação
+    t = []  # datetime_instante
+    n = len(gpx0.tracks[0].segments[0].points)
+
+
+    date_gpx=gpx0.time
+    #print(gpx0.time) #dia/hora da atividade
+    for i in range(0, n, 1):
+        temp= gpx0.tracks[0].segments[0].points[i]
+        t.append(temp.time)
+        lat.append(temp.latitude)
+        lon.append(temp.longitude)
+        ele.append(temp.elevation)
+        hr.append(int(gpx0.tracks[0].segments[0].points[i].extensions[0][0].text))
+
+    # calculo do tempo com fuso horario
+    fuso = timezone(timedelta(hours=-3))
+    th = []
+    for i in range(0, n, 1):
+        th.append(t[i].astimezone(fuso))
+
+    # tempo acumulado em segundos
+    dt = [0]
+    for i in range(0, n - 1, 1):
+        dt.append((t[i + 1] - t[i]).seconds)
+    ts = np.cumsum(dt)
+
+    s = [0]
+    for i in range(0, n - 1, 1):
+        a = (lat[0 + i], lon[0 + i])
+        b = (lat[1 + i], lon[1 + i])
+        s.append(haversine(a, b) * 1000)  # segmentos
+
+    d = np.cumsum(s)
+    delta_ts=np.diff(ts)
+    delta_d=np.diff(d)
+    delta_d[0]=30
+    with np.errstate(divide='ignore', invalid='ignore'):
+        pace_total=(delta_ts/60.0)/(delta_d*0.001)
+    #identificar pace altos.
+
+    #limpar valores altos e baixos
+    id_high=np.where(pace_total>15.0)
+    temp_pace = np.delete(pace_total,id_high)
+    temp_t = np.delete(delta_ts, [id_high])
+    temp_d = np.delete(delta_d, [id_high])
+
+    id_low=np.where(temp_pace< 3.0)
+    pacer = np.delete(temp_pace, id_low)
+    tsr= np.cumsum(np.delete(temp_t, id_low))
+    dr = np.cumsum(np.delete(temp_d, id_low))
+    if opt is None:
+        pass
+    elif (opt.lower())=='print':
+        print('Total de pontos excluídos H :',len(id_high[0]))
+        print('Total de pontos excluídos L :', len(id_low[0]))
+        print('Tamanho vetor               :',len(pacer))
+        #print('Pace médio                  :',pp_pace(np.mean(pacer)))
+        #print('Pace max                    :',pp_pace(max(pacer)))
+        #print('Pace min                    :',pp_pace(min(pacer)))
+
+
+    return {"t" : tsr, "d" : dr, "p" : pacer,"date":date_gpx,'gpx0':gpx0}
 
 import numpy as np
 
 
-import gpxpy.gpx
+#import gpxpy.gpx
 import matplotlib.pyplot as plt
 from datetime import timezone, timedelta
 from haversine import haversine
 
-plt.close('all')
-
-# gpx_file = open('Morning_Run1.gpx', 'r')
-gpx_file = open('173_365_16_km.gpx', 'r')
-
-gpx0 = gpxpy.parse(gpx_file)
 
 
-hr = []  # batimento cardiaco
-lat = []  # latitude
-lon = []  # longitude
-ele = []  # elevação
-t = []  # datetime_instante
-n = len(gpx0.tracks[0].segments[0].points)  # dimensao do vetor
+file ='343_366_Meia_maratona_OAB.gpx'
 
-for i in range(0, n, 1):
-    temp = gpx0.tracks[0].segments[0].points[i]
-    t.append(temp.time)
-    lat.append(temp.latitude)
-    lon.append(temp.longitude)
-    ele.append(temp.elevation)
-    hr.append(int(gpx0.tracks[0].segments[0].points[i].extensions[0][0].text))
+out=gpxfile_imp2(file)
+gpx=out['gpx0']
+print('')
+track=gpx.tracks[0]
+segment=track.segments[0]
+point=segment.points
+print(len(segment.points))
+A=point[44].extensions[0]
+NAMESPACES={'garmin_tpe': 'http://www.garmin.com/xmlschemas/TrackPointExtension/v1'}
+hr=A.find('garmin_tpe:hr', NAMESPACES).text
+print(point[44])
 
-# calculo do tempo com fuso horario
-fuso = timezone(timedelta(hours=-3))
-th = []
-for i in range(0, n, 1):
-    th.append(t[i].astimezone(fuso))
-
-# tempo acumulado em segundos
-dt = [0]
-for i in range(0, n - 1, 1):
-    dt.append((t[i + 1] - t[i]).seconds)
-ts = np.cumsum(dt)
-
-# calculo da distancia( haversine formule)
-# 2R × sen-¹(√[sen²((θ₂ - θ₁)/2) + cosθ₁ × cosθ₂ ...
-# × sen²((φ₂ - φ₁)/2)])
-s = [0]
-for i in range(0, n - 1, 1):
-    A = (lat[0 + i], lon[0 + i])
-    B = (lat[1 + i], lon[1 + i])
-    s.append(haversine(A, B) * 1000)  # segmentos
-
-d = np.cumsum(s)  # distancia percorrida
-
-df = np.diff(ele)
-df = np.append(df, 0)
-
-# calculo pace
-print(len(ts))
-print(len(d))
+print('tempo       ',point[44].time)
+print('latitude    ', point[44].latitude)
+print('longitude   ', point[44].longitude)
+print('elevação    ', point[44].elevation)
+print('hr          ', hr)
 
 
 
-# v1=[]
-# for i in range(0,int(max(d)/1000)+1):
-#     ind=np.where((d>=i*1000)&(d<=(i+1)*1000))
-#     v1.append(np.sum[np.array(s[ind])])
-#     print(v1)
-print('cansei de fazer')
-
-
-# p=((np.array(v)*3.6)**(-1))*60
-# plt.plot(np.cumsum(d5),v)
-# print(np.mean(p))
-
-# fig, ax = plt.subplots(figsize=(19.20,10.80))
-# fig.canvas.set_window_title('Heart rate')
-# ax.plot(th,hr,'r',label='HR')
-# formatter = DateFormatter('%H:%M:%S',fuso)
-# ax.xaxis.set_major_formatter(formatter)
-# plt.legend()
-# ax.set_ylabel('Heart rate')
-# plt.grid(True)
-
-
-# fig2, ax = plt.subplots(figsize=(19.20,10.80))
-# fig2.canvas.set_window_title('elevacao')
-
-# ax.plot(d,ele,'*k',label='elevacao')
-# plt.legend()
-# ax.set_ylabel('elevacao')
-# plt.grid(True)
-
-
-# plt.plot(d,df)
-
-# pos_count, neg_count = 0, 0
-# for i in df:
-#     print('dff=',i)
-
-
-#     if i >= 0:
-#         pos_count += i
-#         print('soma=',pos_count)
-#         time.sleep(1)
-
-#     else:
-#         neg_count += i
-
-# print("Positive numbers in the list: ", pos_count)
-# print("Negative numbers in the list: ", neg_count)
-
-
-# fig, ax = plt.subplots(figsize=(19.20,10.80))
-
-# fig.canvas.set_window_title('Leitura gpstrack')
-# ax.plot(t,hr,'r',label='heart rate')
-# ax.gca().set_title("1 - tzinfo NO, xaxis_date = NO, formatter=NO")
-
-# #ax.plot(dates_sim,sim - dRMSE,'r--')
-# #ax.plot(dates_sim,sim + dRMSE,'r--')
-# #ax.fill_between(dates_sim, sim - dRMSE, sim + dRMSE, alpha=0.2)
-# #ax.xaxis.set_major_formatter(formatter)
-# #plt.ylim(0,1100)
-# #plt.xlim(pd.Timestamp('2020-03-15'),pd.Timestamp('2020-06-15'))
-# plt.grid('major')
-# ax.set_ylabel('Heart rate')
-# plt.legend()
-
-
-# plt.plot(t,ele)
 
